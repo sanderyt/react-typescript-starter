eebdc2e5b0ca555b4135625ee782bee8662d90f7
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-03-11 23:03:08 +0000

    added react router and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React, { FC } from "react";
 import GlobalStyle from "./styles/globalStyles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
 
 export const App: FC = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <h1>My React Application</h1>
-    </>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  //add a theme here
+  colors: {
+    primaryColor: "tomato"
+  }
+};
