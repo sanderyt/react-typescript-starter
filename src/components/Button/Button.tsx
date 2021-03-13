559312676dcdaf17323c587db354f33c487a11e2
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-03-13 16:19:25 +0000

    added testing to the app

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from "./styles";
+
+const Button = () => (
+  <div data-testid="Button">
+    <h1>Button component</h1>
+  </div>
+);
+
+export default Button;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Button from "../components/Button/Button";
 
 export const Home = () => {
   return (
     <div>
       <h1>Welcome to my React app!!</h1>
       <h2>Starter pack</h2>
+      <Button />
     </div>
   );
 };
